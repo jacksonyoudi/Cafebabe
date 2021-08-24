02c59adbd985b67d902810ef3ff697868e88b34d
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-08-24 23:25:23 +0800

    xml

--- Mybatis/src/main/java/com/jacksonyoudi/mybatis/config/XMLConfigBuilder.java
+++ Mybatis/src/main/java/com/jacksonyoudi/mybatis/config/XMLConfigBuilder.java
@@ -1,4 +1,38 @@
 package com.jacksonyoudi.mybatis.config;
 
+
+import com.jacksonyoudi.mybatis.pojo.Configuration;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Properties;
+
+
+
+
+
 public class XMLConfigBuilder {
+    public Configuration parseConfig(InputStream in) throws DocumentException {
+        Document document = new SAXReader().read(in);
+
+        Element rootElement = document.getRootElement();
+        List<Element> list = rootElement.selectNodes("//property");
+        Properties properties = new Properties();
+
+        for (Element element : list) {
+            String name = element.attributeValue("name");
+            String value = element.attributeValue("value");
+            properties.setProperty(name, value);
+        }
+
+
+        new ComboPooledDataSource()
+
+        return null;
+    }
+
 }
