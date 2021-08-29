4f57671a6e2415f430994eafbb311c733b03d7ed
jacksonyoudi <liangchangyoujackson@gmail.com>
2021-08-29 13:14:23 +0800

    sql

--- Mybatis/src/main/java/com/jacksonyoudi/fm/MybatisTest.java
+++ Mybatis/src/main/java/com/jacksonyoudi/fm/MybatisTest.java
@@ -2,6 +2,7 @@ package com.jacksonyoudi.fm;
 
 import com.jacksonyoudi.fm.dao.IUserDao;
 import com.jacksonyoudi.mybatis.pojo.User;
+import jdk.nashorn.internal.ir.CallNode;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -60,4 +61,51 @@ public class MybatisTest {
     }
 
 
+    @Test
+    public void test3() throws IOException {
+        // Resources工具类，配置文件加载
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqluser_proxy.xml");
+        // 2. 解析配置文件，创建fact
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        // 3. 生产opensession openSession(true) 自动提交
+        SqlSession sqlSession = factory.openSession();
+
+        IUserDao mapper = sqlSession.getMapper(IUserDao.class);
+
+        User user1 = new User();
+        user1.setId(10);
+
+        List<User> all = mapper.findByCondition(user1);
+        for (User user : all) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+
+    }
+
+
+    @Test
+    public void test4() throws IOException {
+        // Resources工具类，配置文件加载
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqluser_proxy.xml");
+        // 2. 解析配置文件，创建fact
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        // 3. 生产opensession openSession(true) 自动提交
+        SqlSession sqlSession = factory.openSession();
+
+        IUserDao mapper = sqlSession.getMapper(IUserDao.class);
+
+        int[] arr = {1, 2};
+
+        List<User> all = mapper.findByIds(arr);
+        for (User user : all) {
+            System.out.println(user);
+        }
+
+        sqlSession.close();
+
+    }
 }
--- Mybatis/src/main/java/com/jacksonyoudi/fm/dao/IUserDao.java
+++ Mybatis/src/main/java/com/jacksonyoudi/fm/dao/IUserDao.java
@@ -7,4 +7,10 @@ import java.util.List;
 
 public interface IUserDao {
     public List<User> findAll() throws IOException;
+
+    // 多条件组合查询
+    public List<User> findByCondition(User user);
+
+    // 多值查询
+    public List<User> findByIds(int[] ids);
 }
