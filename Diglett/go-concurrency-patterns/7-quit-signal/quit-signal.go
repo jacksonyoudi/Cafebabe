5ee9c3524e518f78cb5979684bea08b03f29db84
youdi <liangchangyoujackson@gmail.com>
2021-12-17 18:33:18 +0800

    quit signal

--- Diglett/go-concurrency-patterns/7-quit-signal/quit-signal.go
+++ Diglett/go-concurrency-patterns/7-quit-signal/quit-signal.go
@@ -1,1 +1,44 @@
-package __quit_signal
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func boring(msg string, quit chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s %d", msg, i):
+				// do someting
+			case <-quit:
+				fmt.Println("clean up")
+				quit <- "see you"
+				return
+			}
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
+}
+
+
+// 通过 chan将 退出信号 传递到 boring里面
+func main() {
+	quit := make(chan string)
+	c := boring("joe", quit)
+
+	// 读取3次
+	for i := 3; i >= 0; i-- {
+		fmt.Println(<-c)
+	}
+	quit <- "Bye"
+	// boring 会停止
+	fmt.Println("Joe say", <-quit)
+
+	// 退出， boring也会退出
+
+}
