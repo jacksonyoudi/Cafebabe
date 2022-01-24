bbaec0fb95649cc3fa4895d2739efdf19c274777
youdi <liangchangyoujackson@gmail.com>
2022-01-24 09:50:27 +0800

    future

--- Squirrel/src/main/scala/com/jacksonyoudi/concurrent/Demo.scala
+++ Squirrel/src/main/scala/com/jacksonyoudi/concurrent/Demo.scala
@@ -1,5 +1,48 @@
 package com.jacksonyoudi.concurrent
 
-class Demo {
+import scala.util.Try
+
+object Demo {
+  // Some type aliases, just for getting more meaningful method signatures:
+  type CoffeeBeans = String
+  type GroundCoffee = String
+
+  case class Water(temperature: Int)
+
+  type Milk = String
+  type FrothedMilk = String
+  type Espresso = String
+  type Cappuccino = String
+
+  // dummy implementations of the individual steps:
+  def grind(beans: CoffeeBeans): GroundCoffee = s"ground coffee of $beans"
+
+  def heatWater(water: Water): Water = {
+    water.copy(temperature = 85)
+  }
+
+  def frothMilk(milk: Milk): FrothedMilk = s"frothed $milk"
+
+  def brew(coffee: GroundCoffee, heatedWater: Water): Espresso = "espresso"
+
+  def combine(espresso: Espresso, frothedMilk: FrothedMilk): Cappuccino = "cappuccino"
+
+  // some exceptions for things that might go wrong in the individual steps
+  // (we'll need some of them later, use the others when experimenting with the code):
+  case class GrindingException(msg: String) extends Exception(msg)
+
+  case class FrothingException(msg: String) extends Exception(msg)
+
+  case class WaterBoilingException(msg: String) extends Exception(msg)
+
+  case class BrewingException(msg: String) extends Exception(msg)
+
+  // going through these steps sequentially:
+  def prepareCappuccino(): Try[Cappuccino] = for {
+    ground <- Try(grind("arabica beans"))
+    water <- Try(heatWater(Water(25)))
+    espresso <- Try(brew(ground, water))
+    foam <- Try(frothMilk("milk"))
+  } yield combine(espresso, foam)
 
 }
